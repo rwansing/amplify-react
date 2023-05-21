08a5215e10b162f2f9fa253493928bdd4b1f0bc9
Randy Wansing <randal.wansing@outlook.com>
2023-05-21 10:23:13 -0500

    changes for v2

--- src/App.js
+++ src/App.js
@@ -6,14 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
     );
   }
 }
